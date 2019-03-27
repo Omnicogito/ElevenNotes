1cee30abf8c6cada98c875d136dd0436581fa52f
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-03-27 13:38:06 -0400

    Converting Title to String

---
+++ ElevenNote.WebMVC/Models/NoteCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.WebMVC.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage ="Please enter more than two characters")]
+        [MaxLength(100, ErrorMessage ="There are too many characters in this field")]
+        public string Title { get; set; }
+        [MaxLength(8000)]
+        public string Content { get; set; }
+
+        public override string ToString() => Title;
+
+    }
+}
